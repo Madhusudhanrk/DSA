bb239cf7df936873f9a03990bad3067ea81ae06a
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-04 22:41:21 +0530

    about problem solving tips

--- DSA02_problem_solving.py
+++ DSA02_problem_solving.py
@@ -1,5 +1,11 @@
 # Reverse a string
-name = "Madhusudhan"
+#Tips
 
-print(name[-3:-5:-1])
- 
+"""
+1.Read question don't freeze
+2.Understand question in your phrase.
+3.Break down the problem into simple peices
+4.create use cases
+5.try to re use what u learned in the problem.
+
+"""

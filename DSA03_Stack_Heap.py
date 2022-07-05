782067c94599dd98679fb775d63d134e086aebb3
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-05 21:42:09 +0530

    stack and heap learnings

---
+++ DSA03_Stack_Heap.py
@@ -0,0 +1,11 @@
+"""
+A program may run in three stages in memory like:
+
+1.A program itself loading like compiler of the language and vital things to run code.
+
+2.Stack  = This is a temproray memory usually keep in memory like variables, expressions and values in local scope based on which part of code running. if entire module running it memory all varibales and expressions.
+
+3.Heap  = stack uses the Heap to store values of variable, variable types, address of the variables in memory is heap work
+
+
+"""

c98fd32b9123a3eb8a13c2a88c1dd9122ca02fbe
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-06 19:22:07 +0530

    wrapping stack and heap

--- DSA03_Stack_Heap.py
+++ DSA03_Stack_Heap.py
@@ -1,29 +1,70 @@
 """
-A program may run in three stages in memory like:
+Memory process and types of memory:
 
-1.A program itself loading like compiler of the language and vital things to run code.
+A program may run in three stages in memory(RAM) like:
 
-2.Stack  = This is a temproray memory usually keep in memory like variables, expressions and values in local scope based on which part of code running. if entire module running it memory all varibales and expressions.
+1. Code loader => Application when clicked entier code will load.
 
-3.Heap  = stack uses the Heap to store values of variable, variable types, address of the variables in memory is heap work
+2. Stack => stack takes place when compiler load the code and provide a space for two things:
+            * variables (object)(any type)  -> name,address of it
+
+3. Heap  => Heap contain actual values of the object. stack will refer to heap for values using address of objects in stack. 
 
 
 """
-
-"""
-Key Differences Between Stack and Heap Allocations 
  
+# About stack
+# -----------
 
-In a stack, the allocation and de-allocation are automatically done by the compiler whereas in heap, it needs to be done by the programmer manually.
+a = 10
 
-Handling of Heap frame is costlier than the handling of the stack frame.
+b = 10 # Both a and b value is same so only one value stored in heap
 
-Memory shortage problem is more likely to happen in stack whereas the main issue in heap memory is fragmentation.
+print(id(a))
+print(id(b))# is same because the address is stored in stack, the value is stored in heap,value only one copy because python won't create another same value instead it map reference to it.
 
-Stack frame access is easier than the heap frame as the stack have a small region of memory and is cache-friendly, but in case of heap frames which are dispersed throughout the memory so it causes more cache misses.
 
-A stack is not flexible, the memory size allotted cannot be changed whereas a heap is flexible, and the allotted memory can be altered.
+a,b = 11,12
+#now a and b assigned to new values now new address created in stack and heap got new values and the old value 10 is not pointed to any reference address, reference_count is 0. 
 
-Accessing time of heap takes is more than a stack.
+#now python trigger garbage collector
+
+#GARBAGE COLLECTOR:
+
+#garbage collector will automatically collect the values which has 0 reference count so allocating and deallocating is done by PVM programmer no need to worry about stack memory free up.
+
+#call stack 
+
+# it is a small space in stack where every functions takes place. inside the values of function will stored in stack and heap memory when the func calls, when func return all will be removed. 
+def stack(): 
+    x = 10
+    print(x)
+
+stack()
+
+#life of function and its values in memory only when function calls and when it return it will be poped from memory same for classes and methods(fucntions).
+
+#stack fill memory error = stack overflow
+
+
+#Heap
+
+#heap is just can't be access directly using code we can using pointer not recommended.
+
+#heap memory  == RAM memory in computer
+
+#As said heap stores the values of the program and pass values when the stack needed by reference
+
+
+#heap fills full error = out of memory
+
+"""
+Parameter                               STACK                           HEAP
+
+Basic                      Memory is allocated in a         Memory is allocated in any random order.
+                           contiguous block.	
+
+
+for more difference refer geeks for geeks
 
 """

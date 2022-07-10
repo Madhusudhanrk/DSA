8035785c8a90cc775a112a0852f4e1687a3565da
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-10 23:06:31 +0530

    file structuring

--- DSAO8_tower_of_hanoi.py
+++ DSAO8_tower_of_hanoi.py
@@ -1,11 +1,11 @@
 
 # intro
 
-Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules: 
+# Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules: 
 
-Only one disk can be moved at a time.
-Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
-No disk may be placed on top of a smaller disk
+# Only one disk can be moved at a time.
+# Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+# No disk may be placed on top of a smaller disk
 
 
 # Recursive Python function to solve tower of hanoi
@@ -24,6 +24,7 @@ TowerOfHanoi(n, 'A', 'C', 'B')
 
 # Contributed By Harshit Agrawal
 
+"""
 
 Let rod 1 = 'A', rod 2 = 'B', rod 3 = 'C'.
 An example with 2 disks :
@@ -32,9 +33,11 @@ Step 1 : Shift first disk from 'A' to 'B'.
 Step 2 : Shift second disk from 'A' to 'C'.
 
 Step 3 : Shift first disk from 'B' to 'C'.
+"""
 
-
-The pattern here is :
+# The pattern here is :
+"""
  - Shift 'n-1' disks from 'A' to 'B', using C.
  - Shift last disk from 'A' to 'C'.
  - Shift 'n-1' disks from 'B' to 'C', using A.
+ """

5543014d9755344d6b3c3daf32d32cd08f36d939
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-11 23:22:02 +0530

    About factorial

--- DSA06_recursion.py
+++ DSA06_recursion.py
@@ -25,3 +25,6 @@ def factorial(n):
 
 res = factorial(4)
 print(res)
+
+# About factorial
+# In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. The factorial of n also equals the product of n with the next smaller factorial: For example, The value of 0! is 1, according to the convention for an empty product.

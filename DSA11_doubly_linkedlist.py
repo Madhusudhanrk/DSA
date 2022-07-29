183433a3a093b786512e7951bd5b87a098baf3fb
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-30 00:06:23 +0530

    doubly linked push in hitesh method

--- DSA11_doubly_linkedlist.py
+++ DSA11_doubly_linkedlist.py
@@ -11,16 +11,33 @@ class DLinkedList:
     def push(self,data):
         #tip: push works on little bit mess work on paper then focus on the solution in paper.
         new_node = Node(data)
-        if self.head is None:
-            new_node.prev = None
-            new_node.next = None
-            self.head = new_node
-        else:
-            prev_node = self.head
-            new_node.next = prev_node
-            prev_node.prev = new_node
-            new_node.prev = None
-            self.head = new_node
+        
+        #Hitesh Mehod - find patterns complete in less lines
+        new_node.next = self.head  
+
+        #write down in paper and check any similarity values for fill next in node using any already defined variables like in this case head
+        #head holds newly created node here matches with every new node next.
+#pro tip: write down on paper find any pattern in next and prev posistions finish in less lines.
+        if self.head is not None:
+            self.head.prev = new_node
+            #here head.prev starting from 2nd node use newly creating node to assign.
+
+        self.head = new_node
+
+
+        # My Method
+        # if self.head is None:
+        #     new_node.prev = None
+        #     new_node.next = None
+        #     self.head = new_node
+        # else:
+        #     prev_node = self.head
+        #     new_node.next = prev_node
+        #     prev_node.prev = new_node
+        #     new_node.prev = None
+        #     self.head = new_node
+
+        
     
     def append(self,data):
         #tip:always keep prev node reference

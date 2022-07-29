5cd6a2664a7d9faf2e71e8315248e237521d3e54
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-29 23:31:29 +0530

    doubly linked list push

--- DSA11_doubly_linkedlist.py
+++ DSA11_doubly_linkedlist.py
@@ -8,11 +8,19 @@ class DLinkedList:
     def __init__(self):
         self.head = None
 
-    # def push(self,data):
-    #     new_node = Node(data)
-    #     new_node.prev = self.head
-    #     new_node.next = self.head 
-    #     self.head = new_node
+    def push(self,data):
+        #tip: push works on little bit mess work on paper then focus on the solution in paper.
+        new_node = Node(data)
+        if self.head is None:
+            new_node.prev = None
+            new_node.next = None
+            self.head = new_node
+        else:
+            prev_node = self.head
+            new_node.next = prev_node
+            prev_node.prev = new_node
+            new_node.prev = None
+            self.head = new_node
     
     def append(self,data):
         #tip:always keep prev node reference
@@ -39,10 +47,16 @@ class DLinkedList:
 
 
 dlist = DLinkedList()
-dlist.append(10)
-dlist.append(20)
-dlist.append(30)
-dlist.append(40)
-dlist.append(50)
+# dlist.append(10)
+# dlist.append(20)
+# dlist.append(30)
+# dlist.append(40)
+# dlist.append(50)
+
+dlist.push(10)
+dlist.push(20)
+dlist.push(30)
+dlist.push(40)
+dlist.push(50)
 
 dlist.printlist()

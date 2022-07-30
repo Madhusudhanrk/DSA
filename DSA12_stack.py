ea99631b7f0dbbafe0f22bc62b9b104a8b24dd5c
madhusudhan <madhusudhanrk98@gmail.com>
2022-07-30 19:28:13 +0530

    stack in dsa

---
+++ DSA12_stack.py
@@ -0,0 +1,25 @@
+# stack contains 3 key words mainly:
+
+#1.Push - pushing the value into stack one by one
+
+#2.Pop - Delete the value from end to first value.
+
+# stack follows LIFO concept
+
+#LIFO - Last In First Out which value pushed last the need to be deleted first.
+
+#main_point: In stack insertion and deletion works on only one side
+
+#push in python append used
+numbers = []
+numbers.append(10)
+numbers.append(20)
+numbers.append(30)
+
+#pop in python have inbuilt method
+
+numbers.pop()
+numbers.pop()
+numbers.pop()
+#pop deletes from end of the list
+

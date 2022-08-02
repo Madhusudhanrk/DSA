3dab54acd21b265709aa8570fdecbc0906271c3c
madhusudhan <madhusudhanrk98@gmail.com>
2022-08-02 19:22:13 +0530

    BST insertion theory

---
+++ DSA15_binary_search_tree.py
@@ -0,0 +1,23 @@
+# Binary Search Tree (BST)
+# -------------------------------------------------------------------------
+
+# BST INSERTION:
+# ---------------
+
+# 3 Things for insertion:
+
+# 1. Main Tree variable 
+# 2. Small values stored as LEFT child
+# 3. Big values stored as RIGHT child
+ 
+# eg: 11 20 13 8 53 31 21 10 50
+
+# 11 is main tree root
+# now 20 > 11 now 20 becomes right child to 11
+# now 13 > 11, 20 > 11 now 13 becomes left child to 20
+# now 8 < 11 now 8 becomes right child to 11
+
+# Further tree design check with Notes
+
+
+

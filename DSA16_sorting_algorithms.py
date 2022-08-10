1d6649a18c62489eef91065c312a6930271edefc
madhusudhan <madhusudhanrk98@gmail.com>
2022-08-10 17:22:48 +0530

    selection sort my code

--- DSA16_sorting_algorithms.py
+++ DSA16_sorting_algorithms.py
@@ -1,27 +1,23 @@
-# A Sorting Algorithm is used to rearrange a given array or list elements according to a comparison operator on the elements. The comparison operator is used to decide the new order of elements in the respective data structure.
+from array import *
 
 # Selection sort
+ 
+class Sort:
+    def selection_sort(self,arr):
+        # 10 , 6 , 1, 4, 5
+        for i in range(len(arr)):
+            arrow_at = i
+            for j in range(len(arr)):
+                if arr[arrow_at] < arr[j]:
+                    arr[arrow_at],arr[j] = arr[j],arr[arrow_at]
+        self.sorted_values(arr)
 
-# Python program for implementation of Selection
-# Sort
-import sys
-A = [64, 25, 12, 22, 11]
- 
-# Traverse through all array elements
-for i in range(len(A)):
-     
-    # Find the minimum element in remaining
-    # unsorted array
-    min_idx = i
-    for j in range(i+1, len(A)):
-        if A[min_idx] > A[j]:
-            min_idx = j
-             
-    # Swap the found minimum element with
-    # the first element       
-    A[i], A[min_idx] = A[min_idx], A[i]
- 
-# Driver code to test above
-print ("Sorted array")
-for i in range(len(A)):
-    print("%d" %A[i],end=" ")
+    def sorted_values(self,data):
+        for i in data:
+            print(i,end=" ")
+
+
+sort = Sort()
+arr = array("i",[11, 10, 2, 22, 6, 1, 4, 5])
+sort.selection_sort(arr)
+# sort.sorted_values()

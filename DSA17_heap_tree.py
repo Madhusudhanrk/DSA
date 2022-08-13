4c02981d2b5fafd989cacaf361ef3a32c9bfdff7
madhusudhan <madhusudhanrk98@gmail.com>
2022-08-13 23:38:18 +0530

    heap deletion

--- DSA17_heap_tree.py
+++ DSA17_heap_tree.py
@@ -19,3 +19,10 @@ Industry prefer: MAX
 
 #Deletion In Heap Tree
 
+"""
+Step 1 − Remove root node.
+Step 2 − Move the last element of last level to root.
+Step 3 − Compare the value of this child node with its parent.
+Step 4 − If value of parent is less than child, then swap them.
+Step 5 − Repeat step 3 & 4 until Heap property holds.
+"""

582e066dd617a07e5ab20d1f0da9cebc982aafd6
madhusudhan <madhusudhanrk98@gmail.com>
2022-08-14 20:58:42 +0530

    step for heap tree

--- DSA17_heap_tree.py
+++ DSA17_heap_tree.py
@@ -31,3 +31,9 @@ Step 5 − Repeat step 3 & 4 until Heap property holds.
 # We shall use the same example to demonstrate how a Max Heap is created. The procedure to create Min Heap is similar but we go for min values instead of max values.
 
 # We are going to derive an algorithm for max heap by inserting one element at a time. At any point of time, heap must maintain its property. While insertion, we also assume that we are inserting a node in an already heapified tree.
+
+# Step 1 − Create a new node at the end of heap.
+# Step 2 − Assign new value to the node.
+# Step 3 − Compare the value of this child node with its parent.
+# Step 4 − If value of parent is less than child, then swap them.
+# Step 5 − Repeat step 3 & 4 until Heap property holds.

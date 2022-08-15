97eeea8ad04c6acb0789e353ae9c35e47fa310f8
madhusudhan <madhusudhanrk98@gmail.com>
2022-08-15 19:49:54 +0530

    about heap tree as per storage

--- DSA17_heap_tree.py
+++ DSA17_heap_tree.py
@@ -1,4 +1,6 @@
 #HEAP TREE 
+
+#heap is used to store data in storage in the form of array using heap tree structure.
 """
 1.Heap tree is like binary tree but doesn't mantain lower value should be this or upper value this side.
 
@@ -87,20 +89,21 @@ class MinHeap:
 
     #To get index of parent, left and right child indexs based on param index from array--------------------------------------------------------------------
 
+#using the formula it get index of the parent, but we need to give the child index
     def getParentIndex(self,index):
         return (index-1)//2
-
+#if we give parent index it will give left child index, even it in right branch if u give parent index if that parent has left node it will give the index.
     def getLeftChildIndex(self,index):
         return (2*index)+1
-
-    def getLeftChildIndex(self,index):
+#getting right child index same.
+    def getRightChildIndex(self,index):
         return (2*index)+2
     
     #To check the given index has parent, left, right Nodes --------------------------------------------------------------------
-
+#checking has parent if only one root node means getParentIndex() will return -1
     def hasParent(self,index):
         return self.getParentIndex(index) >= 0
-
+#if we give parent index it will compare
     def hasLeftChild(self,index):
         return self.getLeftChildIndex(index) >= self.size
 
@@ -108,10 +111,10 @@ class MinHeap:
         return self.getRightChildIndex(index) >= self.size
     
     #To return the value of the given index --------------------------------------------------------------------
-
+#if we send child index based on that it return the value of the parent value.
     def parent(self,index):
         return self.storage[self.getParentIndex(index)]
-
+#same give parent index return get left child value.
     def leftChild(self,index):
         return self.storage[self.getLeftChildIndex(index)]
 
@@ -129,11 +132,14 @@ class MinHeap:
         if self.isFull():
             return print("Heap is Full")
         self.storage[self.size] = data
-        self.size += 1
+        #every time value inserted size++ to get actual values count in tree
+        self.size += 1 
+        #heapify means based on MIN or Max choosen values inserted as per rules or construcing heap tree structure.
         self.heapifyUp()
 
     def heapifyUp(self):
+        #index decreased by one beacuse just to compare prev value with it.
         index = self.size - 1
         while (self.hasParent(index) and self.parent(index) > self.storage[index]):
             self.swap(self.getParentIndex(index),index)
-        index = self.getParentIndex(index)
+            index = self.getParentIndex(index)

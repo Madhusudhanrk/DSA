5164e93c7c99fffbcbdcc9d0a82d76c60d8b2190
madhusudhanrk <madhusudhanrk98@gmail.com>
2022-09-23 19:15:22 +0530

    AVL tree Notes

---
+++ DSA18_AVL_tree.py
@@ -0,0 +1,17 @@
+#AVL TREES or Balanced Binary Trees
+#----------------------------------
+
+#In AVL Trees Balance is calculated using Height of the tree.
+
+#How to calculate Tree height?
+#Based on stages a node has 1 or 2 leaf nodes then it is stage 1, then leaf nodes having 1 or 2 nodes then it is 2nd stage to root node and 1st stage to child node it like chine link concept this is how to calculate height.
+
+#NOTE: balance factor =  height of left - height of right => -1 , 0 , 1 or not balanced.
+
+#How to calculate Tree is Balanced or Not
+#If tree one side height is high compare to other side this it is imbalanced tree.
+
+#How to balance it?
+#Just make left or right tree nodes should become allign and which side side to length take middle from that length and move to root above that node all nodes need to be moved to other side to balance the tree.
+
+#To balance LL,RR,LR,RL Rotation methods or used how it works i Know.
